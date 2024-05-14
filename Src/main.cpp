19902114bde98f20a781ba8736fe8fbdb067b044
Simonaytes <yandexman228@gmail.com>
2024-05-14 21:36:30 +0300

    Implement simple test in main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,7 +1,24 @@
+#include <thread>
+
 #include "Future.h"
 #include "Promise.h"
 
 int main()
 {
-    
+    Promise<int> promise;
+    Future<int> future = promise.MakeFuture();
+
+    std::thread([&promise]
+                {
+                    int counter = 0;
+                    while(counter != 5000)
+                    {
+                        printf("cnt = %d\n", counter);
+                    }
+                    promise.SetValue(9); 
+                }).detach();
+
+    printf("Start waiting\n");
+    int value = future.Get();
+    printf("Value = $d\n", value);
 }
